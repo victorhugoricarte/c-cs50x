d50988c2f171a1ae971d62af250adb9ea1e95d91
Victor Hugo Ricarte <victorhugorsa19@gmail.com>
2025-06-25 16:11:44 -0300

    Adição de instruções de controle em arquivo 'agree.c'

--- agree.c
+++ agree.c
@@ -5,5 +5,13 @@ char answer;
 int main(void){
     printf("Hi! Answer the question with [y] yes or [n] no:\n\n");
     printf("Do you agree? ");
-    scanf("%c",)
+    scanf("%c", &answer);
+
+    if (answer == 'y'){
+        printf("\nYou answered \"Yes\"\n");
+    } else if (answer == 'n'){
+        printf("\nYou answered \"No\"\n");
+    } else {
+        printf("\nWrong answer. Reload the executable.\n");
+}
 }
