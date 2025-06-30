902aacf7ddea8d85eb9f906b2d12618ea106facf
Victor Hugo Ricarte <victorhugorsa19@gmail.com>
2025-06-29 22:15:59 -0300

    Alteração em condição do loop while em 'cat_while-and-bool.c'

---
+++ cat_while-and-bool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// To declare a boolean variable, you need to include the standard header file <stdbool.h>
+#include <stdbool.h>
+
+int main(void) {
+    bool cat = true;
+    
+    while (cat){
+        printf("meow\n");
+    }
+}
+// This an endless printing loop, press Ctrl+C to break out of the program
