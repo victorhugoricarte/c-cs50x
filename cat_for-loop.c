e57a41499f2ad356cec01ba3ed24235b0012a9f1
Victor Hugo Ricarte <victorhugorsa19@gmail.com>
2025-07-01 23:32:17 -0300

    Adição de comentários e alteração em loop de while para for em arquivo 'cat_for-loop.c'

--- cat_for-loop.c
+++ cat_for-loop.c
@@ -7,11 +7,9 @@ void meow(void){
 
 // An int function returns an integer value at the its end
 int main(void){
-    int i = 0;
-    // To execute a function, put its name together with parenthesis "()" and a semicolon
+    // To execute an already defined function in another you'll need to declare it, put its name together with parenthesis "()" and a semicolon
     // Ex: functionName();
-    while (i < 3){
+    for (int i = 0; i < 3; i++){
         meow();
-        i++;
     }
 }
