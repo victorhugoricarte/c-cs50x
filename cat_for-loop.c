d559e3d149a70f7b2567443b63177af40f490fc2
Victor Hugo Ricarte <victorhugorsa19@gmail.com>
2025-07-17 15:52:52 -0300

    Melhoria nas descrições do arquivo 'cat_for-loop.c'

--- cat_for-loop.c
+++ cat_for-loop.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-// A void function does not return a integer value at the end, being optional
+// A void function does not return a integer value at the end, it is optional.
 void meow(void){
     printf("meow\n");
 }
 
-// An int function returns an integer value at the its end
+// An int function returns an integer value at the its end.
 int main(void){
-    // To execute an already defined function in another you'll need to declare it, put its name together with parenthesis "()" and a semicolon
-    // Ex: functionName();
+    // To declare(nest) an already defined function in another you'll need to declare it, putting its name together with parenthesis "()" and a semicolon ";";
+    // Ex: "theFunctionsName();".
+    // A for loop is a control structure that allows you repeat a block of code a certain number of times using control variable divided in 3 parts;
+    // Ex: "for (initialization; condition; updation) { body }".
     for (int i = 0; i < 3; i++){
         meow();
     }
