31ba9bbaed54028a5cf7b26635dd037c207d39e9
Victor Hugo Ricarte <victorhugorsa19@gmail.com>
2025-07-27 17:16:04 -0300

    Declaração prévia de função já definida + Ajuste nos comentários do arquivo 'cat_for-loop.c'

--- cat_for-loop.c
+++ cat_for-loop.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-// A void function does not return a integer value at the end, it is optional.
-void meow(void){
-    printf("meow\n");
-}
+// - Declaring the existence of a defined function, only this will be enough for the compiler.
+void meow(void);
 
-// An int function returns an integer value at the its end.
+// - An int function returns an integer value at the its end.
 int main(void){
-    // To declare(nest) an already defined function in another you'll need to declare it, putting its name together with parenthesis "()" and a semicolon ";";
-    // Ex: "theFunctionsName();".
-    // A for loop is a control structure that allows you repeat a block of code a certain number of times using control variable divided in 3 parts;
-    // Ex: "for (initialization; condition; updation) { body }".
-    for (int i = 0; i < 3; i++){
+    // - To declare(nest) an already defined function in another you'll need to declare it, putting its name together with parenthesis "()" and a semicolon ";";
+    // - Ex: "theFunctionsName();".
+    // - A for loop is a control structure that allows you repeat a block of code a certain number of times using control variable divided in 3 parts;
+    // - Ex: "for (initialization; condition; updation) { body }".
+    for (int i = 0; i < 10; i++){
         meow();
     }
+}
+
+// - A void function does not return a integer value at the end, it is optional.
+void meow(void){
+    printf("meow\n");
 }
